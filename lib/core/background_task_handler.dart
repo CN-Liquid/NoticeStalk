bfd4c29c98053bdbbbbb41e00965ca418b9a4d2a
CN-Liquid <adhikariaakash90@gmail.com>
2026-07-22 22:49:48 +0545

    Added support for multiple clients

--- lib/core/background_task_handler.dart
+++ lib/core/background_task_handler.dart
@@ -3,28 +3,37 @@ import 'package:workmanager/workmanager.dart';
 import 'package:notice_stalk/repository/repository.dart';
 import 'package:notice_stalk/core/notification.dart';
 
+final clients = ['ioe_exam', 'ioe_pc'];
+
 @pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
     if (task == 'check_for_updates') {
-      final newNotices = await NoticeRepository.instance.fetchNotices();
+      for (final client in clients) {
+        NoticeRepository.instance.setClient(client);
+        final newNotices = await NoticeRepository.instance.fetchNotices();
 
-      if (!newNotices.isSuccess) {
-        NotificationManager.show(
-          title: 'Failed to fetch notices',
-          details: newNotices.error!,
-        );
-        Logger().e(newNotices.error);
-        return Future.value(false);
-      } else if (newNotices.data!.isEmpty) {
-        logger.d('No new notices');
-
-        return Future.value(true);
-      }
-      final notices = newNotices.data!;
-      await NotificationManager.initialize();
-      for (final data in notices) {
-        NotificationManager.show(title: data['details'], details: data['date']);
+        if (!newNotices.isSuccess) {
+          NotificationManager.show(
+            title: 'Failed to fetch notices',
+            details: newNotices.error!,
+          );
+          Logger().e(newNotices.error);
+          return Future.value(false);
+        } else if (newNotices.data!.isEmpty) {
+          logger.d('No new notices');
+          await NotificationManager.initialize();
+          NotificationManager.show(title: 'Notices', details: 'No new notices');
+          return Future.value(true);
+        }
+        final notices = newNotices.data!;
+        await NotificationManager.initialize();
+        for (final data in notices) {
+          NotificationManager.show(
+            title: data['details'],
+            details: data['date'],
+          );
+        }
       }
     }
 
