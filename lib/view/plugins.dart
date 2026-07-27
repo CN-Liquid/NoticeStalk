a67515062c8c8ba1c0c5b4c19a4e790c44475aa5
CN-Liquid <adhikariaakash90@gmail.com>
2026-07-27 22:14:56 +0545

    Changed to  Drawer plugin selection

--- lib/view/plugins.dart
+++ lib/view/plugins.dart
@@ -1,49 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:notice_stalk/repository/repository.dart';
 import 'package:notice_stalk/view/ioe_pc.dart';
 import 'package:notice_stalk/view/ioe_exam.dart';
+import 'package:notice_stalk/view/permissions.dart';
+import 'package:notice_stalk/view/storage.dart';
 
-class PluginsPage extends StatelessWidget {
+class PluginsPage extends StatefulWidget {
   const PluginsPage({super.key});
+
+  @override
+  State<PluginsPage> createState() => _PluginsPageState();
+}
+
+class _PluginsPageState extends State<PluginsPage> {
+  int index = 0;
+  final plugins = [IoeExam(), IoePc()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Plugins Page'), centerTitle: true),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ListTile(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) {
-                        return IoeExam();
-                      },
-                    ),
-                  );
-                },
-                title: Text('IOE Exam'),
-              ),
-              ListTile(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) {
-                        return IoePc();
-                      },
-                    ),
-                  );
-                },
-                title: Text('IOE Pc'),
-              ),
-            ],
-          ),
+      appBar: AppBar(title: Text('Notices')),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('Permissions'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return Permissions();
+                    },
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: Text('Storage'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return StoragePage();
+                    },
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: Text('IOE Exam'),
+              onTap: () {
+                NoticeRepository.instance.setClient('ioe_exam');
+
+                setState(() {
+                  index = 0;
+                });
+              },
+            ),
+            ListTile(
+              title: Text('IOE Pc'),
+              onTap: () {
+                setState(() {
+                  index = 1;
+                });
+              },
+            ),
+          ],
         ),
       ),
+      body: index == 0 ? IoeExam() : IoePc(),
     );
   }
 }
