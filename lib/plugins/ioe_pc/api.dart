d7a1af941f02a43622479e78b6df617f8274e052
CN-Liquid <adhikariaakash90@gmail.com>
2026-08-04 00:03:07 +0545

    Added robust error handling

--- lib/plugins/ioe_pc/api.dart
+++ lib/plugins/ioe_pc/api.dart
@@ -6,6 +6,8 @@ import 'package:notice_stalk/core/api.dart';
 import 'package:notice_stalk/core/notice.dart';
 import 'package:notice_stalk/core/result.dart';
 
+final logger = Logger();
+
 class IoePc extends Api {
   IoePc._private()
     : super(
@@ -22,9 +24,13 @@ class IoePc extends Api {
 
     final modifiedUrl = '$url/${page + 1}';
 
-    final res = await client.get(modifiedUrl);
-
-    document = parse(res.data);
+    try {
+      final res = await client.get(modifiedUrl);
+      document = parse(res.data);
+    } catch (e) {
+      logger.e('Non-Dio Exception: $e');
+      return Result.failure('Unexpected error: $e');
+    }
 
     final data = document.querySelectorAll('.col-md-4.d-flex');
 
@@ -33,15 +39,16 @@ class IoePc extends Api {
         d.querySelector('a')!.attributes['href']!.trim(),
       );
 
-      if (docLink == null) {
-        Logger().e(
-          'failed to retrieve document link : ${d.querySelector('a')!.attributes['href']!.trim()}',
-        );
+      if (!docLink.isSuccess) {
+        Logger().e('failed to retrieve document link : ${docLink.error}');
+        final element = dom.Element.tag('p')..text = null;
+
+        d.append(element);
+      } else {
+        final element = dom.Element.tag('p')..text = docLink.data;
+
+        d.append(element);
       }
-
-      final element = dom.Element.tag('p')..text = docLink;
-
-      d.append(element);
     }
     if (data.isNotEmpty) {
       notices = convertToList(data);
@@ -57,25 +64,29 @@ class IoePc extends Api {
         details: d.querySelector('a')!.text.trim(),
         date: d.querySelector('h7')!.text.trim(),
         link: d.querySelector('a')!.attributes['href']!.trim(),
-        docLink: d.querySelector('p')!.text.trim(),
+        docLink: d.querySelector('p')?.text.trim(),
       );
     }).toList();
   }
 
-  Future<String?> getDocumentLink(String noticeLink) async {
-    final response = await client.get(noticeLink);
-    final document = parse(response.data);
+  Future<Result<String?>> getDocumentLink(String noticeLink) async {
+    dom.Document? document;
     try {
+      final response = await client.get(noticeLink);
+      document = parse(response.data);
+
       final link = document.querySelector(
         '.btn.btn-outline-primary.text-decoration-none.fw-bold',
       );
-      return link!.attributes['href'];
+      return Result.success(link!.attributes['href']);
     } on TypeError {
-      final img = document.querySelector('.wp-image-7207');
+      final img = document!.querySelector('.wp-image-7207');
       if (img == null) {
-        return null;
+        return Result.failure('Unable to extract image');
       }
-      return img.attributes['src'];
+      return Result.success(img.attributes['src']);
+    } catch (e) {
+      return Result.failure('Error occured when retrieving document link $e');
     }
   }
 }
